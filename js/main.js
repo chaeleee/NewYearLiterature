2955f52dad33eb15198fbe2a8f0fb8e906017daa
Chaeyun Lee <hopefulyun@gmail.com>
2019-05-19 02:54:10 +0900

    표어 편집, 이미지 파일 저장 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,41 +1,73 @@
-window.onload=function(){
-}
-
-var canvas = new fabric.Canvas('canvas_ex');
+// var fabricCanvas;
 var painter;
+var articleInputEle;
+var authorInputEle;
+var fabricCanvas;
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    articleInputEle = document.querySelector('#article');
+    authorInputEle = document.querySelector('#author');
+    document.querySelector('#drawBtn').addEventListener('click', ()=>{
+        draw();
+    });
+
+});
+
 
 function draw(){
-    canvas.clear();
-    painter = new Painter(canvas);
-    painter.article = document.querySelector('#article').value;
-    painter.author = document.querySelector('#author').value;
+    fabricCanvas = new fabric.Canvas('canvas_ex');
+    fabricCanvas.clear();
+    fabricCanvas.backgroundColor = 'rgba(255,255,255,1)';
+    painter = new Painter(fabricCanvas, 'canvas_ex', articleInputEle.value, authorInputEle.value);
     painter.drawBoth();
 }
 
 function saveToImg(){
-    let imgUrl = painter.saveToImg();
-    window.open(imgUrl);
+    let uri = painter.getImgUri();
+    // window.open(imgUrl);
+    downloadURI(uri, '제출작.jpg');
 }
 
-var Painter = function(_fabricCanvas){
-    this.fabricCanvas = _fabricCanvas;
-    this.article;
-    this.author;
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+  }
+
+var Painter = function(fabricCanvas, canvasId, articleText, authorText){
+    this.canvasId = canvasId;
+    this.fabricCanvas = fabricCanvas;
+    this.article = articleText;
+    this.author = authorText;
 
     this.articleStyle = {
         fontFamily: 'NanumBarunGothic',
         fontWeight: '800',
         fontSize: '30',
         textAlign: 'center',
+        originX: 'center',
+        originY: 'center',
+        fill: 'black'
     };
     this.authorStyle = {
         fontFamily: 'NanumBarunGothic',
         fontWeight: '500',
-        fontSize: '25',
+        fontSize: '20',
         textAlign: 'center',
+        originX: 'center',
+        originY: 'center',
+        fill: 'rgb(102, 102, 102)'
     }
 
-    this.saveToImg = function(){
+    this.clear = function(){
+        if(this.fabricCanvas !== null) this.fabricCanvas.clear();
+    }
+
+    this.getImgUri = function(){
         this.fabricCanvas.isDrawingMode = false;
         // return this.fabricCanvas.toDataURL('png');
         return this.fabricCanvas.toDataURL( {format: 'jpeg', quality: 0.8});
@@ -48,19 +80,48 @@ var Painter = function(_fabricCanvas){
     }
 
     this.drawArticle = function(){
-        let canvasEle = document.querySelector('#canvas_ex');
-        this.articleStyle['top'] = canvasEle.height/5;
-        let articleText = new fabric.Text(this.article, this.articleStyle);
+        let canvasEle = document.getElementById(this.canvasId);
+        this.articleStyle['top'] = (canvasEle.height/5)*2;
+        let articleText = new fabric.IText(this.article, this.articleStyle);
+        articleText.on('changed', ()=>{
+            let text = articleText.get('text');
+            this.updateInputValue(text, articleInputEle);
+        });
+        
         this.fabricCanvas.add(articleText);
         this.fabricCanvas.centerObjectH(articleText);
+        this.fabricCanvas.renderAll();
         
     }
 
     this.drawAuthor = function(){
-        this.authorStyle['top'] = 200;
-        this.author = '- ' + this.author + ' -';
-        let authorText = new fabric.Text(this.author, this.authorStyle)
+        let canvasEle = document.getElementById(this.canvasId);
+        this.authorStyle['top'] = (canvasEle.height/5)*3.5;
+        this.appendAuthorWings();
+        let authorText = new fabric.IText(this.author, this.authorStyle);
+        authorText.on('changed', ()=>{
+            let text = authorText.get('text');
+            text = this.removeAuthorWings(text);
+            this.updateInputValue(text, authorInputEle);
+        });
+
         this.fabricCanvas.add(authorText);
         this.fabricCanvas.centerObjectH(authorText);
+        this.fabricCanvas.renderAll();
     }
+
+    this.updateInputValue = function(text, input){
+        input.value = text;
+    }
+
+    this.removeAuthorWings = function(text){
+        text = text.replace('- ', '');
+        text = text.replace(' -', '');
+        return text;
+    }
+
+    this.appendAuthorWings = function(){
+        this.author = '- ' + this.author + ' -';
+    }
+
 }
