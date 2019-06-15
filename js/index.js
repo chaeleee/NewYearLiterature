e941b43720bac84dc6f0c30d833c7787ccf1c5ad
chaeyunn <hopefulyun@gmail.com>
2019-06-15 12:00:05 +0900

    리팩토링: 컴포넌트 나누기

--- js/index.js
+++ js/index.js
@@ -1,29 +1,10 @@
 'user strict';
-
-var app = new Vue({
-  el: '#app',
-  mounted() {
-    this.setAniMainTitle();
-    this.setFabricCanvas();
-  },
-  data:{
-    input:{
-      articleText: '',
-      authorText: ''
-    },
-    canvas:{
-      width: 300,
-      height: 300
-    },
-    fabricManager: null
-  },
-  computed: {
-    articleLength: function(){
-      return this.input.articleText.length;
-    }
-  },
+/**
+ * 메인 타이틀
+ */
+var MainTitle = {
   methods: {
-    setAniMainTitle: function(){
+    setAniMainTitle(){
       $('.ml3').each(function(){
         $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
       });
@@ -44,7 +25,99 @@ var app = new Vue({
           easing: "easeOutExpo",
           delay: 2000
         });
+    }
+  },
+  mounted() {
+    this.setAniMainTitle();
+  },
+};
+
+/**
+ * 작품 입력 컴포넌트
+ */
+var WorkInput = {
+  props: ['work', 'displayWorkCanvas'],
+  data(){
+    return {}
+  },
+  methods: {
+    limitArticleInput(){
+      let str = this.work.article;
+      let lines = this.work.article.split("\n");
+      if(str.length > 60 | lines > 4){
+        this.work.article = str.substring(0, 59);
+      }
     },
+    limitAuthorInput(){
+      var str = this.work.author;
+      if(str.length > 15){
+        this.work.author = str.substring(0, 14);
+      }
+    },
+    submitClickHandler(){
+      this.$emit('completeInput');
+    },
+
+  },
+  computed: {
+    articleLength: function(){
+      return this.work.article.length;
+     }
+  }
+};
+
+/**
+ * 응모 컴포넌트
+ */
+var WorkSubmit = {
+  props: ['work'],
+  data(){
+    return {
+
+    };
+  },
+  methods: {
+    submitHandler: function(){
+      // if(this.isValidEmail()){
+      //   if(this.isValidPassword()){
+      //     let isSuccess = this.submitWork();
+      //     if(isSuccess){
+      //       alert("소중한 작품 응모 감사합니다");
+      //       this.toggleSubmissionModal();
+      //     }else{
+      //       alert("다시 시도해주세요~");
+      //     }
+      //   }else{
+      //     alert("비밀번호 형식에 맞춰 입력해주세요.")
+      //   }
+      // }else{
+      //   alert("중복된 이메일입니다.")
+      // }
+    },
+  },
+  
+}
+
+
+/**
+ *  캔버스 이미지화 컴포넌트
+ */
+var WorkCanvas = {
+  props:{
+    'work': Object,
+    'displayWorkCanvas': Boolean,
+  },
+  data() {
+    return {
+      canvas:{
+        width: 300,
+        height: 300
+      },
+      fabricManager: null,
+      downloadBtn: {isHide: true},
+    }
+  },
+  methods: {
     saveToImg: function(){
       let uri = this.fabricManager.getImgUri();
       this.downloadURI(uri, '제출작.png');
@@ -66,37 +139,72 @@ var app = new Vue({
       document.body.removeChild(link);
       link = null;
     },
-    limitArticleInput: function(){
-      let str = this.input.articleText;
-      let lines = this.input.articleText.split("\n");
-      if(str.length > 60 | lines > 4){
-        this.input.articleText = str.substring(0, 59);
-      }
-      this.fabricManager.updateArticleText(this.input.articleText);
-    },
-    limitAuthorInput: function(){
-      var str = this.input.authorText;
-      if(str.length > 15){
-        this.input.authorText = str.substring(0, 14);
-      }
-      this.fabricManager.updateAuthorText(this.input.authorText);
-    },
-    toggleModal: function(){
-      let isSuccess = true;
-      if(isSuccess){
-        $('#submission').modal('toggle');
-      }
-    },
-    showCanvas: function(){
-      $("#step-2").slideToggle("slow");
-    },
-    submissionClick: function(){
-      this.fabricManager.updateArticleText(this.input.articleText);
-      this.fabricManager.updateAuthorText(this.input.authorText);
-    },
     initCanvas: function(){
       this.fabricManager.initItext();
+    },
+    toggleSubmissionModal: function(){
+        $('#submission').modal('toggle');
+        this.downloadBtn.isHide = true;
+        this.showCanvas();
+    },
+  },
+  watch:{
+    'work.article': function(article){
+      this.fabricManager.updateArticleText(article);
+    },
+    'work.author': function(author){
+      this.fabricManager.updateAuthorText(author);
     }
+  },
+  mounted() {
+    this.setFabricCanvas();
+  },
+}
 
-  }
+/**
+ * ROOT
+ */
+var app = new Vue({
+  el: '#app',
+  components: {
+    'main-title': MainTitle,
+    'work-input': WorkInput,
+    'work-submit': WorkSubmit,
+    'work-canvas': WorkCanvas,
+  },
+  data:{
+      work:{
+        article: "",
+        author: ""
+      },
+      displayWorkCanvas: false,
+  },
+  methods: {
+    articleInput(articleText){
+      this.work.article = articleText;
+    },
+    authorInput(authorText){
+      this.work.author = authorText;
+    },
+    completeInput(){
+      $('#submission').modal('toggle');
+      // 공모작 이미지 변환 컴포넌트 텍스트 갱신
+    },
+    toggleCanvas(){
+      if(this.displayWorkCanvas){
+        $("#step-2").slideUp("slow");
+        this.displayWorkCanvas = false;
+      }else{
+        $("#step-2").slideDown("slow");
+        this.displayWorkCanvas = true;
+      }
+    },
+  },
+  mounted() {
+    
+  },
 });
+
+
+
+
