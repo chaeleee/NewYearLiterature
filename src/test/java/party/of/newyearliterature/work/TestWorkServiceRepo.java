dc5e7afd10a47e57b1afca8bda7544eec38d05ad
Chaeyun Lee <hopefulyun@gmail.com>
2019-07-15 09:26:14 +0900

    작업 service-repository 테스트

--- src/test/java/party/of/newyearliterature/work/TestWorkServiceRepo.java
+++ src/test/java/party/of/newyearliterature/work/TestWorkServiceRepo.java
@@ -1,10 +1,16 @@
 package party.of.newyearliterature.work;
 
+import static org.junit.Assert.assertFalse;
+
+import java.util.Objects;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import party.of.newyearliterature.user.UserDto;
 
 /**
  * TestWorkServiceRepo
@@ -17,9 +23,17 @@ public class TestWorkServiceRepo {
 
     @Test public void 작업과유저등록_작업과유저ID반환(){
         // Given
-        // UserDto userDto = new UserDto();
+        UserDto userDto = new UserDto();
+        userDto.setEmail("email");
+        userDto.setNickname("nickname");
+        WorkDto workDto = new WorkDto();
+        workDto.setArticle("article");
+        workDto.setAuthor("author");
+        workDto.setUserDto(userDto);
         // When
-        // WorkDto resDto = workService.submit(workDto);
+        WorkDto resDto = workService.submit(workDto);
         // Then
+        assertFalse(Objects.isNull(resDto.getId()));
+        assertFalse(Objects.isNull(resDto.getUserDto().getId()));
     }
 }
