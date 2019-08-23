609a887ce6124fdd04508785c7c595f03f9682c5
chaeyunn <hopefulyun@gmail.com>
2019-08-23 23:41:43 +0900

    작품 추천: Security 세팅 중 [미완]

---
+++ src/main/java/party/of/newyearliterature/security/BasicConfiguration.java
@@ -0,0 +1,44 @@
+package party.of.newyearliterature.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * BasicConfiguration
+ */
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+            .inMemoryAuthentication()
+            .withUser("user")
+                .password("password")
+                .roles("USER")
+                .and()
+            .withUser("admin")
+                .password("admin")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    // @Override
+    // protected UserDetailsService userDetailsService(){
+
+    // }
+    
+}
